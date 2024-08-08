8896de8e3ed33220852687006e9fa75d12642f62
unknown <cpravin7276@gmail.com>
2024-08-08 23:01:17 +0530

    increase speed

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -2,7 +2,7 @@ import math
 
 def reward_function(params):
     '''
-    Optimized reward function for AWS DeepRacer on Rose Raceway (counterclockwise)
+    Reward function optimized for Rose Raceway (counterclockwise)
     '''
     # Read input parameters
     track_width = params['track_width']
@@ -32,32 +32,30 @@ def reward_function(params):
 
         # Reward based on distance from center
         if distance_from_center <= marker_1:
-            reward += 2.0  # Higher reward for staying close to the center
+            reward += 1.2
         elif distance_from_center <= marker_2:
-            reward += 1.0
+            reward += 0.8
         elif distance_from_center <= marker_3:
             reward += 0.5
-        else:
-            reward += 0.1  # Small reward for staying on track even if close to the edge
 
         # Penalize for high steering angles to prevent zig-zagging
         if steering_angle > 15:
             reward *= 0.7
 
-        # Apply a moderate penalty for sharp turns at high speed, but not too severe
-        HIGH_SPEED_THRESHOLD = 3.5
-        SHARP_TURN_THRESHOLD = 15  # Adjusted to allow more aggressive driving
+        # Penalize sharp turns at high speed
+        HIGH_SPEED_THRESHOLD = 3.0
+        SHARP_TURN_THRESHOLD = 15  # degrees
         if steering_angle > SHARP_TURN_THRESHOLD and speed > HIGH_SPEED_THRESHOLD:
-            reward *= 0.7  # Reduced penalty to allow higher speeds
+            reward *= 0.4
 
-        # Reward higher speeds with a strong emphasis
+        # Reward for maintaining a good speed
         SPEED_THRESHOLD = 3.5
         if speed >= SPEED_THRESHOLD:
-            reward += 2.0
-        elif speed >= 3.0:
             reward += 1.5
         elif speed >= 2.5:
-            reward += 1.0
+            reward += 0.8
+        elif speed >= 2.0:
+            reward += 0.5
 
         # Reward progress efficiently
         if steps > 0:
